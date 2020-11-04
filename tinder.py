bf610548935ca10340084549ebde2830091f8f7c
antony <52021528+antonysandesh@users.noreply.github.com>
2020-11-04 23:22:48 +0530

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -1,11 +1,13 @@
 import pyautogui
 import time
 
-cursour_location = pyautogui.position()
+#put cursor on the location to click before running this code.
+cursour_location = pyautogui.position() 
+
 # >>> Point(x=1457, y=894)
 
 i=0
-while i < 10:
+while i < 10: #number of times to be clicked 
     pyautogui.click(cursour_location)
     speed.time(0.2)
     i += 1     
